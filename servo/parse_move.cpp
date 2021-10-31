24b60c9cd87ce6f9d4a4e3518b7801d05f373bf8
Tianda Huang <huangtianda@gmail.com>
2021-10-30 20:42:01 -0500

    added input validation and comments

--- servo/parse_move.cpp
+++ servo/parse_move.cpp
@@ -6,15 +6,28 @@ char panStr[4] = {0, 0, 0, 0};
 char tiltStr[4] = {0, 0, 0, 0};
 char data[10];
 
-static inline int threeDigitNum(const char* str) {
-  return 100 * (str[0] - '0') + 10 * (str[1] - '0') + (str[2] - '0');
+/**
+ * turns a 3-length number c-string into an integer
+ * validates output for servo angles (0 degrees to 180)
+ * if number is invalid, returns 90
+ * null terminator not needed
+ */
+static int threeDigitNum(const char* str) {
+  int output = 100 * (str[0] - '0') + 10 * (str[1] - '0') + (str[2] - '0');
+  if (output < 0 || output > 180) output = 90;
+  return output;
 }
 
+/**
+ * parse_input parses Serial input and converts to two angles
+ * Serial input expects two angles given in three digits, concatenated
+ *  ie. angles 45 and 90 : Serial 045090
+ */
 void parse_input(int *panAngle, int *tiltAngle) {
 	if (Serial.available() > 0) {
 		int it = Serial.readBytesUntil('\0', data, 9);
     if (DEBUG) {
-		  Serial.println(it);
+		  // Serial.println(it);
     }
 		memcpy(panStr, &data[0], 3 * sizeof(char));
 		memcpy(tiltStr, &data[3], 3 * sizeof(char));
--- servo/parse_move.h
+++ servo/parse_move.h
@@ -1,6 +1,11 @@
 #ifndef PARSE_MOVE_H
 #define PARSE_MOVE_H
 
+/**
+ * parse_input parses Serial input and converts to two angles
+ * Serial input expects two angles given in three digits, concatenated
+ *  ie. angles 45 and 90 : Serial 045090
+ */
 void parse_input(int *panAngle, int *tiltAngle);
 
 #endif // PARSE_MOVE_H
