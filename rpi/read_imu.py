580814f3e570a411963ec08b5bcce85c21bdbaf8
Aditya <aditya.narayanan03@gmail.com>
2021-10-31 02:15:24 -0500

    Sending motor angles to arduino over serial

--- rpi/read_imu.py
+++ rpi/read_imu.py
@@ -1,6 +1,6 @@
 from mpu_lib import *
-import time
-import sys
+from math_utils import *
+import os, sys, serial, time
 
 mpu = MPU9250()
 
@@ -8,9 +8,20 @@ X_BIAS = 1.0198036 - 1.0
 Y_BIAS = 1.0061316 - 1.0
 Z_BIAS = 1.0335 - 1.0
 accel = {'x': 0.0, 'y': 0.0, 'z': 0.0}
-rotation = {'theta':90, 'psi':90}
+
+dev = [x[2] for x in os.walk('/dev')]
+for file in dev[0]:
+	if 'USB' in file:
+		port = file
+		break;
+print(port)
+ser = serial.Serial('/dev/' + port, 115200, timeout=1)
 
 while True:
 	accel['x'] = mpu.readAccel()['x'] - X_BIAS
 	accel['y'] = mpu.readAccel()['y'] - X_BIAS
 	accel['z'] = mpu.readAccel()['z'] - X_BIAS
+	angles = angles_from_xyz(accel)
+	print('{}{}\0\n'.format(str(int(round(angles[0]))).zfill(3), str(int(round(angles[1]))).zfill(3)))
+	ser.write('{}{}\0'.format(str(int(round(angles[0]))).zfill(3), str(int(round(angles[1]))).zfill(3)))
+	time.sleep(.010)
